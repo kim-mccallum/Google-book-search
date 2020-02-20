17ef90f09170bb5be70cf1cd55884e8daaa2207e
Kim McCallum <kchaase@gmail.com>
2020-02-20 16:39:08 -0700

    added API call

--- src/SearchForm/SearchForm.js
+++ src/SearchForm/SearchForm.js
@@ -5,35 +5,50 @@ import './SearchForm.css';
 // Most complex component - Gets the parameters, (Key word search, apply search filters) and formats and makes the request
 class SearchForm extends Component {
     // Add methods for calling the API within an onSubmit method
-    handleSubmit(e){
-        debugger;
-        console.log(e.target.value)
-        // API stuff - add this soon but first get the values from the form
+    handleSubmit = (event) => {
         const url = 'https://www.googleapis.com/books/v1/volumes?';
-        const apiKEY = 'AIzaSyC1-pejzfhSrLcjQatjyYXkJ5fwGLmWmNM';
-        
-    }
+        const apiKEY  = 'AIzaSyC1-pejzfhSrLcjQatjyYXkJ5fwGLmWmNM';
+        event.preventDefault();
+        // Generate request
+        const request = url + 'q=' + event.target.SearchText.value + '&' + event.target.bookType.value + '&' + event.target.printType.value + '&key=' + apiKEY;
+        console.log(request)
+
+        fetch(request)
+            .then(res => {
+            if(!res.ok) {
+                throw new Error('Something went wrong, please figure this out.');
+            }
+            return res;
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+            .catch(err => {
+                console.log('oops!')
+            });
+      }
 
     render() {
         // Return/render a form with event handlers to pass data back to SearchScreen
         return (
             <div className="SearchForm">
                 <form onSubmit={e => this.handleSubmit(e)}>
-                    <label htmlFor="Search">Search:</label>
+                    <label htmlFor="SearchText">Search:</label>
                     <input 
                         type="text" 
-                        name="Search" 
+                        name="SearchText" 
                         id="Search" 
                         placeholder="henry"/>
                     <button type="submit" >Search</button>
-                    <label htmlFor="print-type">Print Type</label>
-                    <select id="print-type" name="print-type">
+                    <label htmlFor="printType">Print Type</label>
+                    <select id="printType" name="printType">
                         <option value="printType=all">all</option>
                         <option value="printType=books">books</option>
                         <option value="printType=magazines">magazines</option>
                     </select>
-                    <label htmlFor="book-type">Book Type</label>
-                    <select id="book-type" name="book-type">
+                    <label htmlFor="bookType">Book Type</label>
+                    <select id="bookType" name="bookType">
                         <option value="">No Filter</option>
                         <option value="filter=partial">partial text</option>
                         <option value="filter=full">full text</option>
